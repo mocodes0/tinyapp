8de43b52c8543a74db11e50c1fa5cedd65c78927
Mohamed Bashir <mo.dev416@gmail.com>
2021-12-08 06:59:34 +0000

    Add a GET Route to Show the Form, Added Body Parser, Generated Random String Function

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
 const express = require("./node_modules/express");
 const app = express();
 
-const PORT = 8080;
-
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = 8080;
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -21,8 +24,27 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: "" };
-  res.render("urls_show", templateVars);
+  res.render("urls_show", templateVars); // Page of a Single URL
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+function generateRandomString() {
+  let url = "";
+  const characterSet =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 6; i++) {
+    url += characterSet.charAt(Math.floor(Math.random() * characterSet.length));
+  }
+  return url;
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
